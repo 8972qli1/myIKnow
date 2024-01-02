83529626848cde5c508d4174511ff8f8bc57de62
qli1 <qli1@ebay.com>
2024-01-02 15:45:36 +0800

    init

---
+++ src/main/java/com/example/myiknow/MyIKnowApplication.java
@@ -0,0 +1,13 @@
+package com.example.myiknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyIKnowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyIKnowApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/myiknow/api/TestAPI.java
@@ -0,0 +1,18 @@
+package com.example.myiknow.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestAPI {
+
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test(){
+        return "test success";
+    }
+
+}
